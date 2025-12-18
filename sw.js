0723206e8a1d8f136086bf056defa7f717914539
DevDW <dweminem@gmail.com>
2025-12-18 09:52:33 -0300

    fix: Update service worker zone ID
    The service worker configuration for V3ndas Online was using an incorrect zone ID. This commit updates the `zoneId` in `sw.js` to the correct value, ensuring proper service worker functionality and ad serving.

--- sw.js
+++ sw.js
@@ -5,3 +5,9 @@ self.options = {
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10344281
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
